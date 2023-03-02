13a5ca3b9e4aa8f1e73b789ccd4f379081b93c94
Liplum <Li_plum@outlook.com>
2023-03-03 02:09:33 +0800

    installSourceMap

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { type Request, type Response } from "express"
+import { install as installSourceMap } from "source-map-support"
+
+installSourceMap()
 
 const app = express()
 
-async function startServer() {
+async function startServer(): Promise<void> {
   app.listen(80)
 }
-
